f8ee2e99223bad8a274183f9c5edb75e679acfef
adriswara <chevellerz454@yahoo.com>
2018-02-13 19:29:24 +0700

    moved const camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Camera, CameraOptions } from '@ionic-native/camera';
-import {GoogleMaps,GoogleMap,GoogleMapsEvent,GoogleMapOptions,CameraPosition,MarkerOptions,Marker} from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, CameraPosition, MarkerOptions, Marker } from '@ionic-native/google-maps';
 
 
 @NgModule({
@@ -16,7 +16,7 @@ import {GoogleMaps,GoogleMap,GoogleMapsEvent,GoogleMapOptions,CameraPosition,Mar
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [Camera,GoogleMaps]
+  providers: [Camera, GoogleMaps]
 })
 
 //export class AppModule {}
@@ -26,19 +26,59 @@ export class HomePage {
   //private geolocation: Geolocation
   //private camera: Camera
   map: GoogleMap;
-  constructor(public navCtrl: NavController, private camera: Camera,private googleMaps: GoogleMaps) {}
-//camera
+  constructor(public navCtrl: NavController, private camera: Camera, private googleMaps: GoogleMaps) {}
+  //camera
   const options: CameraOptions = {
     quality: 100,
     destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
+
+
+
   }
-  
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+    this.map = this.googleMaps.create('map_canvas', mapOptions);
+
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        console.log('Map is ready!');
+
+        // Now you can use all methods safely.
+        this.map.addMarker({
+          title: 'Ionic',
+          icon: 'blue',
+          animation: 'DROP',
+          position: {
+            lat: 43.0741904,
+            lng: -89.3809802
+          }
+        })
+          .then(marker => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK)
+              .subscribe(() => {
+                alert('clicked');
+              });
+          });
+
+      });
+  }
 }
-
-
-
 
 /* GEO LOCATION ERROR getCurrentPosition not found
  this.geolocation.getCurrentPosition().then((resp) => {
