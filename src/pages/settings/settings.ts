8a2f490e990bf28029e729f52c0bdce621559dc8
ilhamandri <ilhamandriandri@gmail.com>
2018-03-01 14:44:50 +0700

    Firebase bug fixed

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseServiceProvider } from './../../providers/firebase-service/firebase-service';
-import { FirebaseListObservable } from 'angularfire2/database';
+//import { FirebaseListObservable } from 'angularfire2/database';
 
+import { AngularFireObject } from 'angularfire2/database';
+import { AngularFireList } from 'angularfire2/database/interfaces';
 /**
  *new bug,unsuported plugin angularFire and firebase
  */
@@ -14,7 +16,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
-  trainData: FirebaseListObservable<any[]>;
+  trainData: AngularFireList<any[]>;
   newItem = '';
   constructor(public navCtrl: NavController, public navParams: NavParams,public firebaseService: FirebaseServiceProvider) {
     this.trainData = this.firebaseService.getTrainList();
