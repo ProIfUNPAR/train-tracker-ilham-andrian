ca7a6835d15cd707a2d9d26145ce29053bde6bb6
adriswara <chevellerz454@yahoo.com>
2018-04-05 09:16:24 +0700

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -359,6 +359,57 @@ export class HomePage {
 
 }
 
+
+ /* bagian ngitung jarak 
+ angular.module('dynamic-sports.services')
+ .factory('geoLocationService', ['$interval', function ($interval) {
+   'use strict';
+   var watchId;
+
+   return {
+     start: function (success, error) {
+       watchId = $interval(function () {
+         navigator.geolocation.getCurrentPosition(success, error, {enableHighAccuracy: true});
+       }, 1000);
+     },
+     stop: function () {
+       if (watchId) {
+         $interval.cancel(watchId);
+       }
+     }
+   };
+ }]);
+
+
+ function toRad(value) {
+      var RADIANT_CONSTANT = 0.0174532925199433;
+      return (value * RADIANT_CONSTANT);
+    }
+
+    function calculateDistance(starting, ending) {
+      var KM_RATIO = 6371;
+      try {
+        var dLat = toRad(ending.latitude - starting.latitude);
+        var dLon = toRad(ending.longitude - starting.longitude);
+        var lat1Rad = toRad(starting.latitude);
+        var lat2Rad = toRad(ending.latitude);
+
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = KM_RATIO * c;
+        return d;
+      } catch(e) {
+        return -1;
+      }
+    }
+
+*/
+
+
+
+
+
 /* GEO LOCATION ERROR getCurrentPosition not found
  this.geolocation.getCurrentPosition().then((resp) => {
       // resp.coords.latitude
