4ef810209d99b3710600580b6eefe5cac26bdb93
Adriswara <chevellerz454@yahoo.com>
2018-05-02 22:58:02 +0700

    changed view

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-about',
@@ -7,9 +8,9 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  kereta = []
+  constructor(public navCtrl: NavController, public http: Http) {
 
   }
 
 }
-//
